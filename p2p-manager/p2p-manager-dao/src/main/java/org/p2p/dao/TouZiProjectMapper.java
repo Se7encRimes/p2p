ff8d4dfd0b6599b5be2865b7f1305e7e0f3f2c14
Se7encRimes <1148872740@qq.com>
2017-09-18 17:34:27 +0800

    投资记录条件查询

--- p2p-manager/p2p-manager-dao/src/main/java/org/p2p/dao/TouZiProjectMapper.java
+++ p2p-manager/p2p-manager-dao/src/main/java/org/p2p/dao/TouZiProjectMapper.java
@@ -3,8 +3,10 @@ package org.p2p.dao;
 import org.apache.ibatis.annotations.Param;
 import org.p2p.pojo.vo.ProjectVague;
 import org.p2p.pojo.vo.TouZiProject;
+import org.p2p.utlis.InvestItem;
 import org.p2p.utlis.Page;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -17,4 +19,6 @@ public interface TouZiProjectMapper {
     List<TouZiProject> projectVague(@Param("project")ProjectVague project,@Param("page")Page page);
 
     int getPageSize(@Param("project")ProjectVague projectVague);
+
+    List<InvestItem> touziRecord(@Param("id")int id,@Param("starttime")Date seprate_time,@Param("state")int type);
 }
